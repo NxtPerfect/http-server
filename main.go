f67245ba533c0859b51c0df4f4e4bb71ff968292
NxtPerfect <alakaxan@protonmail.com>
2024-08-01 13:34:08 +0200

    Added payload type
    changed server type paths to map
    updated tests
    added code and content to response of path

--- main.go
+++ main.go
@@ -1,5 +1,12 @@
 package main
 
+import (
+	"fmt"
+	"net"
+	"os"
+	"strings"
+)
+
 type Head struct {
 	ContentType     mediaType
 	ContentEncoding contentCoding
@@ -30,55 +37,125 @@ type contentCoding struct {
 }
 
 const (
-  GET = iota
-  POST
-  PUT
-  DELETE
+	GET = iota
+	POST
+	PUT
+	DELETE
 )
 
 type Server struct {
-  port int
+	port          string
+	templatesPath string
+	paths         map[string][]Path
+}
+
+type Path struct {
+	url     string
+	method string
+	value  string
+}
+
+type Payload struct {
+	code    int
+	content string
 }
 
 const (
-  HTTP_OK = 200
-  HTTP_ACCEPTED = 202
-  HTTP_BAD_REQUEST = 400
-  HTTP_UNAUTHORIZED = 401
-  HTTP_FORBIDDEN = 403
-  HTTP_NOT_FOUND = 404
-  HTTP_GONE = 410
-  HTTP_INTERNAL_SERVER_ERROR = 500
+	HTTP_OK                    = 200
+	HTTP_ACCEPTED              = 202
+	HTTP_BAD_REQUEST           = 400
+	HTTP_UNAUTHORIZED          = 401
+	HTTP_FORBIDDEN             = 403
+	HTTP_NOT_FOUND             = 404
+	HTTP_GONE                  = 410
+	HTTP_INTERNAL_SERVER_ERROR = 500
 )
 
 func (server *Server) listen() {
-  panic("Not implemented yet")
+	// panic("Not implemented yet")
+  _, err := net.Listen("tcp", server.port)
+  if err != nil {
+    panic("Couldn't start tcp server.")
+  }
 	return
 }
 
-func createServer(port int) (Server, error) {
-  var server Server
-  server.port = port
-  return server, nil
+func createServer(port string, templatesPath string, paths []Path) (Server, error) {
+	var server Server
+	server.port = port
+	server.templatesPath = templatesPath
+	for i := range len(paths) {
+		j := 0 // HACK: iterate from 0 to 4
+		if server.paths[paths[i].url][j].method == paths[i].method {
+			errorMessage := fmt.Sprintf("Trying to override existing url %s for method %s with value %s, make sure your config is correct/", paths[i].url, paths[i].method, paths[i].value)
+			panic(errorMessage)
+		}
+		// TODO: check if url exists
+		// if yes, append to method and value
+		// else create new path
+		if server.paths[paths[i].url] != nil {
+			server.paths[paths[i].url] = append(server.paths[paths[i].url], createPath(paths[i].url, paths[i].method, paths[i].value))
+		}
+		server.paths[paths[i].url] = append(server.paths[paths[i].url], createPath(paths[i].url, paths[i].method, paths[i].value))
+	}
+	return server, nil
 }
 
-func (server *Server) addPath(path string, method int) (error) {
-  // server.paths = { path: path, method: method }
-  panic("Not implemented yet")
-  return nil
+// Default server on port 1337 with templatse path /templates and get root path /
+// TODO: update to new server architecture (paths having arrays)
+func createDefaultServer() (Server, error) {
+	var server Server
+	server.port = "1337"
+	server.templatesPath = "/templates"
+	server.paths["/"] = append(server.paths["/"], createPath("/", "GET", "Hello, World!"))
+	return server, nil
 }
 
-func (server *Server) GET(path string) (int, error) {
-  panic("Not implemented yet")
-  return HTTP_OK, nil
+// Returns new path object
+func createPath(url string, method string, value string) Path {
+	var path Path
+  path.url = url
+	path.method = method
+	path.value = value
+	return path
 }
 
-func (server *Server) POST(path string, payload string) (int, error) {
-  panic("Not implemented yet")
-  return HTTP_OK, nil
+func createPathManyMethods(urls []string, methods []string, values []string) []Path {
+	var paths []Path
+	for i := range len(methods) {
+    paths[i].url = urls[i]
+		paths[i].method = methods[i]
+		paths[i].value = values[i]
+	}
+	return paths
 }
 
-func (server *Server) changePermission(path string, rule string) (error){
-  panic("Not implemented yet")
-  return nil
+// TODO: update to new server architecture (paths having arrays)
+func (server *Server) addPath(url string, method string, returnValue string) error {
+	if strings.HasSuffix(returnValue, ".html") {
+		// return html
+	}
+	htmlFileContent, err := os.ReadFile(server.templatesPath + "/" + returnValue)
+	if err != nil {
+		panic("File doesn't exist or has incorrect access permissions.")
+	}
+	htmlFile := string(htmlFileContent)
+	server.paths[url] = append(server.paths[url], createPath(url, method, htmlFile))
+	panic("Not implemented yet")
+	return nil
+}
+
+func (server *Server) GET(path string) (Payload, error) {
+	panic("Not implemented yet")
+  return Payload{code: HTTP_OK, content: "No content"}, nil
+}
+
+func (server *Server) POST(path string, payload string) (Payload, error) {
+	panic("Not implemented yet")
+  return Payload{code: HTTP_OK, content: "No content"}, nil
+}
+
+func (server *Server) changePermission(path string, rule string) error {
+	panic("Not implemented yet")
+	return nil
 }
--- main_test.go
+++ main_test.go
@@ -1,67 +1,75 @@
 package main
 
-import "testing"
+import (
+	"os"
+	"testing"
+)
 
 func TestValidPath(t *testing.T) {
-  server, err := createServer(4043)
-  if err != nil {
-    t.Fatalf(`Failed to create server.`)
-  }
-  server.listen()
-  code, err := server.GET("/")
-  if err != nil {
-    t.Fatalf(`Failed to send get.`)
-  }
-  if code != HTTP_OK {
-    t.Fatalf(`Requested GET for '/' failed with %d`, code)
-  }
+	server, err := createServer("4043", "/temaplates", []Path{createPath("/", "GET", "index.html")})
+	if err != nil {
+		t.Fatalf(`Failed to create server.`)
+	}
+	server.listen()
+	response, err := server.GET("/")
+	if response.code != HTTP_OK {
+		t.Fatalf(`Requested GET for '/' failed with %d.`, response.code)
+	}
+	correctContent, err := os.ReadFile("./templates/index.html")
+	if err != nil {
+		t.Fatalf(`Couldn't find correct file.`)
+	}
+	correctHtml := string(correctContent)
+	if response.content != correctHtml {
+		t.Fatalf(`Requested data is empty`)
+	}
 }
 
 func TestUnauthorized(t *testing.T) {
-  server, err := createServer(4043)
-  if err != nil {
-    t.Fatalf(`Failed to create server.`)
-  }
-  server.addPath("/admin", GET)
-  server.changePermission("/admin", "Role: Admin")
-  server.listen()
-  code, err := server.GET("/admin")
-  if err != nil {
-    t.Fatalf(`Failed to send get.`)
-  }
-  if code != HTTP_UNAUTHORIZED {
-    t.Fatalf(`Requested GET for '/admin' without permissions accepted with %d`, code)
-  }
+	server, err := createServer("4043", "/temapltes", []Path{createPath("/admin", "GET", "You're an admin")})
+	if err != nil {
+		t.Fatalf(`Failed to create server.`)
+	}
+	server.changePermission("/admin", "Role: Admin")
+	server.listen()
+	response, err := server.GET("/admin")
+	if err != nil {
+		t.Fatalf(`Failed to send get.`)
+	}
+	if response.code != HTTP_UNAUTHORIZED {
+		t.Fatalf(`Requested GET for '/admin' without permissions accepted with %d`, response.code)
+	}
+  // TODO: check content
 }
 
 func TestPostWithPayload(t *testing.T) {
-  server, err := createServer(4043)
-  if err != nil {
-    t.Fatalf(`Failed to create server.`)
-  }
-  server.addPath("/login", POST)
-  server.listen()
-  code, err := server.POST("/login", "username: user, password: pass")
-  if err != nil {
-    t.Fatalf(`Failed to send post.`)
-  }
-  if code != HTTP_OK {
-    t.Fatalf(`Requested POST for '/login' with payload failed with %d`, code)
-  }
+	server, err := createServer("4043", "/templates", []Path{createPath("/login", "POST", "Post without value")})
+	if err != nil {
+		t.Fatalf(`Failed to create server.`)
+	}
+	server.listen()
+	response, err := server.POST("/login", "username: user, password: pass")
+	if err != nil {
+		t.Fatalf(`Failed to send post.`)
+	}
+	if response.code != HTTP_OK {
+		t.Fatalf(`Requested POST for '/login' with payload failed with %d`, response.code)
+	}
+  // TODO: check content
 }
 
 func TestPostWithoutPayload(t *testing.T) {
-  server, err := createServer(4043)
-  if err != nil {
-    t.Fatalf(`Failed to create server.`)
-  }
-  server.addPath("/login", POST)
-  server.listen()
-  code, err := server.POST("/login", "")
-  if err != nil {
-    t.Fatalf(`Failed to send post.`)
-  }
-  if code != HTTP_BAD_REQUEST {
-    t.Fatalf(`Requested POST for '/login' with no data accepted with %d`, code)
-  }
+	server, err := createServer("4043", "/template", []Path{createPath("/login", "POST", "Post with payload")})
+	if err != nil {
+		t.Fatalf(`Failed to create server.`)
+	}
+	server.listen()
+	response, err := server.POST("/login", "")
+	if err != nil {
+		t.Fatalf(`Failed to send post.`)
+	}
+	if response.code != HTTP_BAD_REQUEST {
+		t.Fatalf(`Requested POST for '/login' with no data accepted with %d`, response.code)
+	}
+  // TODO: check content
 }
